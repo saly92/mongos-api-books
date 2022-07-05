5582e98e954e0b804c5f483d46f86f1cfb7218d5
sali <sali.taymor@gmail.com>
2022-07-05 11:08:23 +0200

    import finished

---
+++ cli/importBooks.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { Book } from "../models/Book.js";
+import axios from "axios";
+
+mongoose.connect("mongodb://localhost/bookapi");
+
+const url = "https://edwardtanguay.netlify.app/share/books.json";
+
+console.log("connected to mongoose");
+
+const rawBooks = (await axios.get(url)).data;
+
+for (const rawBook of rawBooks) {
+    console.log(`processing ${rawBook.title}...`);
+    const book = new Book({
+        title: rawBook.title,
+        description: rawBook.description,
+        numberOfPages: Number(rawBook.totalpages),
+        language: rawBook.language,
+        imageUrl: `http://edwardtanguay.netlify.app/share/images/books/${rawBook.idcode}.png`,
+        buyUrl: rawBook.buyUrl,
+    });
+    console.log(book)
+    await book.save();
+}
+
+console.log("imported completed");
+process.exit(1);
